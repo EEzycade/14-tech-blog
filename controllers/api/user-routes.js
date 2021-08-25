25051da254f47c8d9ff84cb4b338064426af2721
Cade Ellsworth <caedmd2100@gmail.com>
2021-08-24 19:58:29 -0700

    server up and running, basic page displayed on server start

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        });
+});
+
+router.get('/', (req, res) => {
+    // Access our User model and run .findAll() method
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
