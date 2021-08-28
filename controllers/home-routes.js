d31bb0f0cc0833c8457deac1d1ed5b30a5c6c663
Cade Ellsworth <caedmd2100@gmail.com>
2021-08-27 18:25:26 -0700

    added user routes, post routes, and comment routes(not functional), added login js file, added styles

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('home');
+    console.log(req.session);
+    res.render('home', {
+        loggedIn: req.session.loggedIn
+    });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
